bceb0e42a9440ed4c25489896e36df938412be42
EncisoRafaelMartin <60369657+EncisoRafaelMartin@users.noreply.github.com>
2020-06-15 18:58:57 -0500

    1

--- UNIDAD_11/Texto_Imagen/Text_Ima_Main.java
+++ UNIDAD_11/Texto_Imagen/Text_Ima_Main.java
@@ -3,8 +3,8 @@ package UNIDAD_11.Texto_Imagen;
 import javax.swing.JFrame;
 
 public class Text_Ima_Main {
-    public static void main(String args[]) {
-        Texto_Imagenes panelEtiqueta = new Texto_Imagenes();
+    public static void main(final String args[]) {
+        final Texto_Imagenes panelEtiqueta = new Texto_Imagenes();
         panelEtiqueta.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panelEtiqueta.setSize(500, 700);
         panelEtiqueta.setVisible(true);
